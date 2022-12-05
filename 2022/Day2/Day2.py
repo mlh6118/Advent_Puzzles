9077e8676ba0648db4c8a6cbbdcdbebc0d7a2821
Marni Hager <mlh6118@gmail.com>
2022-12-04 21:56:18 -0800

    Completed Day 2 Part 2

--- 2022/Day2/Day2.py
+++ 2022/Day2/Day2.py
@@ -9,27 +9,35 @@ while True:
     Line = Line.split(' ')
 
     if Line[1] == 'X':
-        total_score += 1
-    elif Line[1] == 'Y':
-        total_score += 2
-    else:
-        total_score += 3
-
-    if Line[1] == 'X':
         if Line[0] == 'A':
-            total_score += 3
-        elif Line[0] == 'C':
-            total_score += 6
-    elif Line[1] == 'Y':
-        if Line[0] == 'B':
-            total_score += 3
-        elif Line[0] == 'A':
-            total_score += 6
-    else: # Line[1] = 'Z'
-        if Line[0] == 'C':
+            # Score Scissors losing to Rock
             total_score += 3
         elif Line[0] == 'B':
+            # Score Rock losing to Paper
+            total_score += 1
+        elif Line[0] == 'C':
+            # Score Paper losing to Scissors
+            total_score += 2
+    elif Line[1] == 'Y':
+        if Line[0] == 'A':
+            # Score Rock (1 + 3)
+            total_score += 4
+        elif Line[0] == 'B':
+            # Score Paper (2 + 3)
+            total_score += 5
+        elif Line[0] == 'C':
+            # Score Scissors (3 + 3)
             total_score += 6
+    else:  # Line[1] = 'Z'
+        if Line[0] == 'A':
+            # Score Paper win over Rock (2 + 6)
+            total_score += 8
+        elif Line[0] == 'B':
+            # Score Scissors win over Paper (3 + 6)
+            total_score += 9
+        elif Line[0] == 'C':
+            # Score Rock win over Scissors (1 + 6)
+            total_score += 7
 
 print(total_score)
 
