618cdb2627de5950c27debad41fba3e35543c0df
Marni Hager <mlh6118@gmail.com>
2023-09-27 14:49:47 -0700

    Day 4 Part 2 is now complete.

--- 2022/Day4/main.py
+++ 2022/Day4/main.py
@@ -13,23 +13,57 @@ def Cleanup():
 
         for line in Lines:
             line = line.strip()
-            print(line)
 
             text = re.split('-|,', line)
-            print(text)
 
             if int(text[0]) >= int(text[2]):
                 if int(text[1]) <= int(text[3]):
-                    print("True")
                     total = total + 1
                     continue
 
             if int(text[0]) <= int(text[2]):
                 if int(text[3]) <= int(text[1]):
-                    print("True")
                     total = total + 1
                     continue
 
         return total
 
 print(Cleanup())
+
+
+def Cleanup_Overlap():
+    with open('input.txt', 'r') as file:
+        Lines = file.readlines()
+
+        overlap = False
+        total_overlaps = 0
+
+        for line in Lines:
+            line = line.strip()
+
+            text = re.split('-|,', line)
+
+            if int(text[0]) >= int(text[2]):
+                if int(text[0]) <= int(text[3]):
+                    overlap = True
+                    total_overlaps = total_overlaps + 1
+                    continue
+            elif int(text[1]) <= int(text[3]):
+                if int(text[1]) >= int(text[2]):
+                    overlap = True
+                    total_overlaps = total_overlaps + 1
+                    continue
+            elif int(text[2]) <= int(text[0]):
+                if int(text[2]) <= int(text[1]):
+                    overlap = True
+                    total_overlaps = total_overlaps + 1
+                    continue
+            elif int(text[3]) <= int(text[1]):
+                if int(text[3]) >= int(text[0]):
+                    overlap = True
+                    total_overlaps = total_overlaps + 1
+                    continue
+
+    return total_overlaps
+
+print(Cleanup_Overlap())
